0865893a7e990ce4c8164c3b6cbfc41628f6f762
Yattish Ramhorry <yattish@gmail.com>
2018-06-22 17:53:38 +0200

    added new page for sports and recreation details

---
+++ src/pages/item-create-event-sports-rec-detail/item-create-event-sports-rec-detail.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ItemCreateEventSportsRecDetailPage } from './item-create-event-sports-rec-detail';
+
+@NgModule({
+  declarations: [
+    ItemCreateEventSportsRecDetailPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ItemCreateEventSportsRecDetailPage),
+  ],
+})
+export class ItemCreateEventSportsRecDetailPageModule {}
